b3f7a34465b0f77c58c8e0da454e7d48366d17f6
Mas4035 <mason.cabral@students.dominican.edu>
2023-01-30 17:35:53 -0800

    Initial Commit

---
+++ Magic8Ball4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic8Ball4
+//
+//  Created by Student Guest on 1/30/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
